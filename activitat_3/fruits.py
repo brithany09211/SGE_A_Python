43a96025fa297dcb6e8c049a56e21d1494e7bfa1
brithany09211 <smb09211@gmail.com>
2024-11-26 10:12:53 +0100

    Agregados los ejercicios en la carpeta activitat_3

---
+++ activitat_3/fruits.py
@@ -0,0 +1,6 @@
+#Imprimir els elements d'una llista amb for. fruits = [“poma”,”pera”,”raïm”,”plàtan”]
+
+fruits = ["poma", "pera", "raïm", "plàtan"]
+print("Els fruits són:")
+for i in fruits:
+    print(f"{i}")
---
+++ activitat_3/imprimir_numeros_for_while.py
@@ -0,0 +1,12 @@
+#Imprimir números de l’1 al 10 (amb for i amb while)
+
+print("Imprimint números de l'1 al 10 amb for:")
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+for i in num:
+    print(f"El número és: {i}")
+
+print("Imprimint números de l'1 al 10 amb while:")
+i = 1
+while i <= 10:
+    print(f"El número és: {i}")
+    i += 1
